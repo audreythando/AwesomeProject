85c827a3861f544bfd88056bffb2d32c99697548
audreythando <audreythando@gmail.com>
2022-10-11 11:49:12 +0200

    refactor: added text and styleSheet

--- components/Tasks.js
+++ components/Tasks.js
@@ -1,3 +1,17 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
+const Task =() => {
+    <View>
+        <Text>
+            This is a task 
+        </Text>
+    </View>
+}
+
+
+const styles= StyleSheet.create({
+
+
+})
+export default Task;
