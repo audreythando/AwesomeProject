31e67b2487cdeb7143da0b1c709674c27fa55aa3
audreythando <audreythando@gmail.com>
2022-10-11 11:33:06 +0200

    refactor:removed unwanted code.

--- App.js
+++ App.js
@@ -4,8 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View styles={styles.tasksWrapper}>
+
+<Text style={styles.sectionTitle}>Today's tasks</Text>
+   
+   <View style={styles.items}>
+
+   </View>
+   
+   
+    </View>
     </View>
   );
 }
@@ -13,8 +21,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  tasksWrapper:{
+    padding:50,
+    paddingHorizontal:20,
+  },
+  sectionTitle:{
+    fontSize:24,
+    fontWeight:'bold',
+    marginTop:60,
+  },
+  items:{},
 });
